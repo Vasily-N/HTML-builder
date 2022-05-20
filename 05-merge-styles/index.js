d3ff54fc0b53f89e122ed23779a9322103072b9a
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-20 15:01:13 +0300

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { createReadStream, createWriteStream, promises: { readdir } } = require('fs');
+
+const alt = process.argv.includes('-t');
+
+const dirOut = path.join(__dirname, alt ? 'test-files' : 'project-dist');
+const pathOut = path.join(dirOut, 'bundle.css');
+const dirIn = path.join(__dirname, alt ? 'test-files' : '', 'styles');
+
+const copyFileDataToStream = (path, ws) => {
+  const rs = createReadStream(path);
+  rs.pipe(ws, { end: false });
+  return new Promise(resolve => rs.on('close', resolve));
+};
+
+const mergeStyles = async(src, dest) => {
+  const mergeFile = async(file, ws) => {
+    if(!file.isFile() || path.extname(file.name) !== '.css') return;
+    await copyFileDataToStream(path.join(src, file.name), ws);
+    ws.write('\n');
+  };
+
+  const ws = createWriteStream(dest);
+  const files = await readdir(src, {withFileTypes: true});
+  for(const file of files) {
+    await mergeFile(file, ws);
+  }
+};
+
+mergeStyles(dirIn, pathOut);
