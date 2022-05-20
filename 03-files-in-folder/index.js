608c4f957c8cb77fa2efd5c654e7391cce2bc811
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-20 10:29:28 +0300

    refactor: fs.stats => fs.promises.stat in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,9 @@
 const path = require('path');
-const fs = require('fs');
-const { readdir } = fs.promises;
+const { promises: { readdir, stat } } = require('fs');
 
 const dir = path.join(__dirname , 'secret-folder');
 
-const getFileSizeAsync = async filePath =>
-  await new Promise(resolve => {
-    fs.stat(filePath, (_, stats) => resolve(stats.size));
-  });
+const getFileSizeAsync = async filePath => (await stat(filePath)).size;
 
 const getFileSizeFormatAsync = async filePath =>
   `${((await getFileSizeAsync(filePath))/1024).toFixed(3)}kb`;
