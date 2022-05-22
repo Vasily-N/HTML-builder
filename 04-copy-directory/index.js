c0d04339a2f93ad1d19f503664afd6524ca22606
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-22 06:57:39 +0300

    feat: check access in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,31 +1,50 @@
 const path = require('path');
 const { promises: fsp } = require('fs');
 
-const src = path.join(__dirname, 'files');
-const dest = path.join(__dirname, 'files-copy');
-
 const createDirAsync = async(dest) => {
-  if(await fsp.stat(dest).catch(() => false )) {
+  const destNotExist = (await fsp.access(dest).catch(e => e.code));
+  if(!destNotExist) {
     await fsp.rm(dest, { recursive: true });
+  } else if(destNotExist !== 'ENOENT') {
+    return false;
   }
-  await fsp.mkdir(dest, { recursive: true, maxRetries: 10, retryDelay: 100 });
+
+  try {
+    await fsp.mkdir(dest, { recursive: true, maxRetries: 10, retryDelay: 100 });
+  } catch {
+    return false;
+  }
+
+  return true;
 };
 
-const copyDirAsync = async(src, dest) => {
-  await createDirAsync(dest);
-  const files = await fsp.readdir(src, {withFileTypes: true});
+const copyDirAsync = async(dirSrc, dirDest) => {
+  const srcNotExist = await fsp.access(dirSrc).catch(() => true);
+  if(srcNotExist) {
+    process.stdout.write(`No access to ${dirSrc}, abort copyDir`);
+    return;
+  }
+
+  if(!(await createDirAsync(dirDest))) {
+    process.stdout.write(`Something (it can be VSCode!) is blocking access to ${dirDest}, abort copyDir`);
+    return;
+  }
+
+  const files = await fsp.readdir(dirSrc, {withFileTypes: true});
   files.forEach(async(file) => {
-    const fileSrc = path.join(src, file.name);
-    const fileDest = path.join(dest, file.name);
+    const src = path.join(dirSrc, file.name);
+    const dest = path.join(dirDest, file.name);
     if(file.isDirectory())
-      await copyDirAsync(fileSrc, fileDest);
+      await copyDirAsync(src, dest);
     else if(file.isFile())
-      await fsp.copyFile(fileSrc, fileDest);
+      await fsp.copyFile(src, dest);
   });
 };
 
-if(require.main === module)
+if(require.main === module) {
+  const src = path.join(__dirname, 'files');
+  const dest = path.join(__dirname, 'files-copy');
   copyDirAsync(src, dest);
-else {
+} else {
   module.exports = { copyDirAsync, createDirAsync };
 }
