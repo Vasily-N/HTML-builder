dc4560b04dccb9a6583cf778217b45fd3f7908ae
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-23 19:28:06 +0300

    feat: add message about bug in Git Bash

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 
+process.stdout.write('Git Bash for Windows 2.35.1-2.35.4 has a bug: Ctrl+C doesn\'t work. Either update or use another terminal');
+
 const fileIn = path.join(__dirname , 'text.txt');
 
 const writeStream = fs.createWriteStream(fileIn);
