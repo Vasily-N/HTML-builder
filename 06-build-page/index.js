da14fffbc2eb94ac61bc9352b54dd2d596ecd6f8
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-24 17:47:31 +0300

    refactor: 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,11 +3,9 @@ const { promises:  { readFile, readdir, writeFile } } = require('fs');
 const { copyDirAsync, createDirAsync } = require(path.resolve(__dirname, '..', '04-copy-directory'));
 const mergeStylesAsync = require(path.resolve(__dirname, '..', '05-merge-styles'));
 
-const dest = path.join(__dirname, 'project-dist');
-
 const errMessage = msg => process.stdout.write(`Please close a program (can be VSCode's "live server") that blocks ${msg} from updating\n`);
 
-const buildPageAsync = async dest => {
+const buildPageAsync = async (src, dest) => { //todo: custom filenames via options?
   const tryMergeComponentToHTML = (html, componentKey, componentValue) => {
     const toReplace = html.split(new RegExp(`( *?){{${componentKey}}}`, 'g'));
     if(toReplace.length < 2 ) return html;
@@ -15,19 +13,27 @@ const buildPageAsync = async dest => {
     return toReplace.map((v, i) => (i % 2) ? `${v}${componentRows.join(v)}` : v).join('');
   };
 
-  const htmlFileName = 'index.html';
-  const stylesFileName = 'style.css';
+  const templateSrc = path.join(src, 'template.html');
+  const componentsSrc = path.join(src, 'components');
+  const htmlDestFile = 'index.html';
+  const htmlDest = path.join(dest, htmlDestFile);
+
+  const assetsSrc = path.join(src, 'assets');
+  const assetsDestPath = path.join(dest, 'assets');
+
+  const stylesSrc = path.join(src, 'styles');
+  const stylesDestFile = 'style.css';
+
   //using await at the same line as func call is much slower
   const createDirPromise = createDirAsync(dest);
 
-  const templatePromise = readFile(path.join(__dirname, 'template.html'));
+  const templatePromise = readFile(templateSrc);
 
-  const componentsDir = path.join(__dirname, 'components');
-  const filesPromise = readdir(componentsDir, {withFileTypes: true}).catch(() => []);
+  const filesPromise = readdir(componentsSrc, {withFileTypes: true}).catch(() => []);
 
   const componentsPromise = (await filesPromise).reduce((p, file) => 
     file.isFile() ? (fileParse => fileParse.ext === '.html'
-      ? {...p, [fileParse.name]: readFile(path.join(componentsDir, file.name))}
+      ? {...p, [fileParse.name]: readFile(path.join(componentsSrc, file.name))}
       : p)(path.parse(file.name)) : p, {});
 
   const createDirErr = await createDirPromise;
@@ -36,17 +42,16 @@ const buildPageAsync = async dest => {
     return;
   }
 
-  const assetsDestPath = path.join(dest, 'assets');
-  const copyDirSuccessPromise = copyDirAsync(path.join(__dirname, 'assets'), assetsDestPath);
-  const mergeSuccessPromise = mergeStylesAsync(path.join(__dirname, 'styles'), dest, stylesFileName);
+  const copyDirSuccessPromise = copyDirAsync(assetsSrc, assetsDestPath);
+  const mergeSuccessPromise = mergeStylesAsync(stylesSrc, dest, stylesDestFile);
 
   const resultHTML = await Object.keys(componentsPromise).reduce(async(pPromise, c) =>
     tryMergeComponentToHTML(await pPromise, c, (await componentsPromise[c]).toString()), (await templatePromise).toString());
 
   try {
-    await writeFile(path.join(dest, htmlFileName), resultHTML);
+    await writeFile(htmlDest, resultHTML);
   } catch {
-    errMessage(htmlFileName);
+    errMessage(htmlDestFile);
   }
 
   if(!(await copyDirSuccessPromise)) {
@@ -54,8 +59,12 @@ const buildPageAsync = async dest => {
   }
 
   if(!(await mergeSuccessPromise)) {
-    errMessage(stylesFileName);
+    errMessage(stylesDestFile);
   }
 };
 
-buildPageAsync(dest);
+const src = __dirname;
+const dest = path.join(__dirname, 'project-dist');
+
+
+buildPageAsync(src, dest);
